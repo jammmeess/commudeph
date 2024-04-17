99d91e6853e2df49c5bed92f63b1a76fb9ccb6e6
jammmeess <james_bomediano@hotmail.com>
2024-04-17 13:25:11 +0800

    Minor Spacing Fixes

--- src/components/FifthSection.jsx
+++ src/components/FifthSection.jsx
@@ -23,13 +23,6 @@ const FifthSection = () => {
       alt: "Virtual Reality",
       title: "Article Title",
     },
-    {
-      id: 4,
-      imgUrl:
-        "https://images.unsplash.com/photo-1605647540924-852290f6b0d5?q=80&w=1437&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      alt: "Virtual Reality",
-      title: "Article Title",
-    },
   ];
 
   return (
