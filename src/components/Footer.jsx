85a5a9a87f52378a61702037894129ebbc6d994a
jammmeess <james_bomediano@hotmail.com>
2024-04-17 13:25:30 +0800

    Updated Footer Image

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const Footer = () => {
   return (
     <section className="footer">
-   <img src="https://images.unsplash.com/photo-1606857521015-7f9fcf423740?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="footer"  alt="Office"/>
+      <img
+        src="https://images.unsplash.com/photo-1549637642-90187f64f420?q=80&w=1474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        className="footer"
+        alt="Office"
+      />
     </section>
   );
 };
